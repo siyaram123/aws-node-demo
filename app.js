2a726d273e19ae0a4ae7589f910c60788a411e2a
Siyaram Kumar <siyaramkumar@gmail.com>
2026-06-04 12:52:05 +0530

    add changes in file

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   res.send(`
     <h1>Welcome to AWS Jenkins Demo</h1>
     <a href="/about">This is About Me</a><br>
-    <a href="/contact">This is my Contact Details</a><br>
+    <a href="/contact">Contact Details</a><br>
     <a href="/api/users"> This is Users API</a>
   `);
 });
